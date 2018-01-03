1e0f72ed3e4143f351bdfe0d37fb3cae27ddb017
aa57847225 <799095632@qq.com>
2018-01-03 16:04:00 +0800

    spring cloud config -server / client

---
+++ spring-config-git/SpringCloudConfigDemo/src/main/java/com/whl/demo/Application.java
@@ -0,0 +1,12 @@
+package com.whl.demo;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+}
